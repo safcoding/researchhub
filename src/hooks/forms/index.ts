49ef75e166763c604e44f7b2b309616411413547
safcoding <safuanhakim003@gmail.com>
2025-07-17 01:07:38 +0800

    went nuclear

--- src/hooks/forms/index.ts
+++
@@ -1,4 +0,0 @@
-export { useFormLogic } from './useFormLogic';
-export { useEventForm } from './useEventForm';
-export { useLabForm } from './useLabForm';
-export { useGrantForm } from './useGrantForm';
