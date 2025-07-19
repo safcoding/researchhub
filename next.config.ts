f98a2602282f3ed37534ca801d2ebae947954ad4
safcoding <safuanhakim003@gmail.com>
2025-07-19 22:03:56 +0800

    disabled  eslint duirng build

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-/*
+
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },*/
+  },
   images: {
     domains: [
       'mjiit.utm.my',
