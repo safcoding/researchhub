65680f3cffbf67d94f089efbfe196ea70c6b9e11
safcoding <safuanhakim003@gmail.com>
2025-07-21 01:57:11 +0800

    utm sanjungan bangsa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/UTM-LOGO.png" />
+      </head>
       <body
         className={cn("min-h-screen bg-background font-sans antialiased", inter.variable)}
       >
